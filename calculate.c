b85783f4a49073970d2b055e037d2f1dfa97fbc2
Nicolas Le Gall <nicolaslegall34@gmail.com>
2015-02-02 17:15:34 +0100

    Create initMatrice function

--- calculate.c
+++ calculate.c
@@ -1,15 +1,45 @@
 #include "calculate.h"
 
-int calculMatrice(const char* file) {
+int intMatrice(const char* file, int** matrice1, int** matrice2) {
 	FILE *fp;
-	int* x;
-	x = malloc(sizeof(int));
+	int nbMult, x1, x2, y1, y2;
+	int i, j, tmp;
 
 	fp = fopen(file,"r");
-	while(fscanf(fp,"%d",x) == 1)  
+
+	fscanf(fp,"%d",&nbMult);
+	fscanf(fp,"%d",&x1);
+	fscanf(fp,"%d",&y1);
+	fscanf(fp,"%d",&x2);
+	fscanf(fp,"%d",&y2);
+
+	matrice1 = malloc(sizeof(int)*x1);
+	matrice2 = malloc(sizeof(int)*x2);
+
+	for (i = 0; i < x1; i++)
+		*(matrice1+i) = malloc(sizeof(int)*y1);
+	for (i = 0; i < x2; i++)
+		*(matrice2+i) = malloc(sizeof(int)*y2);
+
+	for (i = 0; i < x1; i++)
 	{
-		printf("\n%d", *x);  
+		for (j = 0; j < y1; j++)
+		{
+			fscanf(fp,"%d",&tmp);
+			*(*(matrice1+i)+j) = tmp;
+		}
 	}
+
+	for (i = 0; i < x2; i++)
+	{
+		for (j = 0; j < y2; j++)
+		{
+			fscanf(fp,"%d",&tmp);
+			*(*(matrice2+i)+j) = tmp;
+		}
+	}
+
+	fclose(fp);
 
 	return 0;
 }
