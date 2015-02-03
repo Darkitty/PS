19321624f7733a2f23a25b9824c18996eae1ea19
Nicolas Le Gall <nicolaslegall34@gmail.com>
2015-02-03 17:48:38 +0100

    Remove printf debug

--- generate.c
+++ generate.c
@@ -17,27 +17,20 @@ int generateFile(int nbMult) {
 		x = -1;
 		y = -1;
 		z = -1;
+
 		while(x <= 0)
-		{
 			x = 10.0*(0.5-((double)rand())/((double)RAND_MAX));
-			printf("X : %d\n", x);
-		}
 		while(y <= 0)
-		{
 			y = 10.0*(0.5-((double)rand())/((double)RAND_MAX));
-			printf("Y : %d\n", y);
-		}
 		while(z <= 0)
-		{
 			z = 10.0*(0.5-((double)rand())/((double)RAND_MAX));
-			printf("Z : %d\n", z);
-		}
+
 		fprintf(fp, "%d %d\n", x, y);
 		fprintf(fp, "%d %d\n", y, z);
+
 		generateMatrice(fp, x, y);
 		generateMatrice(fp, y, z);
 	}
-
 	fclose(fp);
 	return 0;
 }
@@ -45,6 +38,7 @@ int generateFile(int nbMult) {
 void generateMatrice(FILE* fp, int x, int y) {
 	int i, j;
 	double tmp;
+	
 	for (i = 0; i < x; i++)
 	{
 		for (j = 0; j < y; j++)
